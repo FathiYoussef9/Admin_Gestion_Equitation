92f63a3f7a40059830d63eb25accbfaffbd7cb87
Fathi youssef <81202992+FathiYoussef9@users.noreply.github.com>
2021-04-30 16:58:44 +0000

    commit

--- Admin_Gestion/app/src/main/java/com/example/admin_gestion/HomeDashboard.java
+++ Admin_Gestion/app/src/main/java/com/example/admin_gestion/HomeDashboard.java
@@ -1,14 +1,40 @@
 package com.example.admin_gestion;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.cardview.widget.CardView;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
-public class HomeDashboard extends AppCompatActivity {
-
+public class HomeDashboard extends AppCompatActivity implements View.OnClickListener{
+    private CardView EmploiTemps,Taches,Seances,Pointage,users;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home_dashboard);
+        //defining Cards
+        EmploiTemps = (CardView) findViewById(R.id.EmploiTempsId);
+        Taches = (CardView) findViewById(R.id.TacheId);
+        Seances = (CardView) findViewById(R.id.SeancesId);
+        Pointage = (CardView) findViewById(R.id.PointageId);
+        users = (CardView) findViewById(R.id.GestionUserId);
+        //Add Click Listener to the cards
+        EmploiTemps.setOnClickListener(this);
+        Taches.setOnClickListener(this);
+        Seances.setOnClickListener(this);
+        Pointage.setOnClickListener(this);
+        users.setOnClickListener(this);
+
+    }
+
+    @Override
+    public void onClick(View v) {
+        Intent i;
+
+        switch (v.getId()){
+            case R.id.EmploiTempsId : i = new Intent(this,emploi_du_temps.class);break;
+            case R.id.TacheId : i = new Intent(this,Taches.class);break;
+        }
     }
 }
---
+++ Admin_Gestion/app/src/main/java/com/example/admin_gestion/emploi_du_temps.java
@@ -0,0 +1,14 @@
+package com.example.admin_gestion;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class emploi_du_temps extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_emploi_du_temps);
+    }
+}
