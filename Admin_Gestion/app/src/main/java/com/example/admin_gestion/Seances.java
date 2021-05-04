9c2b1034e506a9a9737191a480fac15837bf1c45
Fathi youssef <81202992+FathiYoussef9@users.noreply.github.com>
2021-05-04 17:33:18 +0000

    :alien:

--- Admin_Gestion/app/src/main/java/com/example/admin_gestion/Seances.java
+++ Admin_Gestion/app/src/main/java/com/example/admin_gestion/Seances.java
@@ -1,14 +1,39 @@
 package com.example.admin_gestion;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.viewpager.widget.ViewPager;
 
 import android.os.Bundle;
 
+import com.google.android.material.tabs.TabLayout;
+
 public class Seances extends AppCompatActivity {
+
+    private TabLayout tabLayout;
+    private ViewPager viewPager;
+    private ViewPagerAdapter adapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_seances);
+
+
+
+        tabLayout = (TabLayout) findViewById(R.id.tablyout_id);
+        viewPager = (ViewPager) findViewById(R.id.viewpager_id);
+        adapter = new ViewPagerAdapter(getSupportFragmentManager());
+
+        //Add fragments here
+        adapter.AddFragment(new FragmentAllSeance(),"");
+        adapter.AddFragment(new FragmentAddSeance(),"");
+        adapter.AddFragment(new FragmentSearchSeance(),"");
+
+        viewPager.setAdapter(adapter);
+        tabLayout.setupWithViewPager(viewPager);
+
+        tabLayout.getTabAt(0).setIcon(R.drawable.ic_baseline_assignment_turned_in_24);
+        tabLayout.getTabAt(1).setIcon(R.drawable.ic_baseline_add_circle_outline_24);
+        tabLayout.getTabAt(2).setIcon(R.drawable.ic_baseline_search_24);
     }
 }
